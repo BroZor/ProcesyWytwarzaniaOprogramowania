861724ef87ac4d851e7da395e5eea57960cd4684
Jan Kowalski <jan.kowalski@pollub.edu.pl>
2024-10-10 16:55:51 +0200

    fix/main

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.println("Hello World2");
-        System.out.println("nowy commit feat/login");
+        System.out.println("nowy commit feat/login 2");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
